98af39e3f99583649cf01b1bd45289753bf886f6
haroune99 <hrna2011@gmail.com>
2025-02-17 12:58:28 +0000

    Starting this timesheet project

---
+++ Project.ts
@@ -0,0 +1,36 @@
+const projects : Project[] = [];
+
+export class Project {
+    constructor(
+        public leader: string,
+        public name: string,
+        public hours: number,
+        public startDate: Date,
+        public endDate: Date,
+        public description: string,
+        public status: string,
+        public progress: string,
+        public team: string[],
+        public PM: string
+    ) {
+        this.name = name;
+        this.hours = hours;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+        this.status = status;
+        this.progress = progress;
+        this.team = team;
+        this.PM = PM;
+    }
+
+    public static save(project: Project) {
+        projects.push(project);
+    }
+
+    public static getProjects(leader: string) {
+        return projects.filter((project) => (project.leader === leader) || (project.team.includes(leader)));
+    }
+    
+}   
+
