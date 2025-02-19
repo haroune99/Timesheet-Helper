a4840e5746982a49662be36920a7ed49987f0725
haroune99 <hrna2011@gmail.com>
2025-02-19 20:28:19 +0000

    First box

--- frontendapp/src/index.js
+++ frontendapp/src/index.js
@@ -5,19 +5,35 @@ import './index.css';
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Header />
+      <Body />
     </div>
   );
 }
 
 function Header() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="header">
+      <h1>TimeSheet Tracker</h1>
     </div>
   );
 }
 
+function Body() {
+  return (
+    <div className="project-box">
+      <img className="project-image" src="https://waz.smartdraw.com/working-smarter/img/how-to-create-a-project-planning-map.svg?bn=15100111939" alt="Project image" />
+      <h3>Project Name</h3>
+      <div className="button-container">
+        <button className="button">Start</button>
+        <button className="button">Stop</button>
+      </div>
+      <div className="time-container">
+        <span>00:00:00</span>
+      </div>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
